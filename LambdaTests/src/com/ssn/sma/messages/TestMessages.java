82e4cdf31445c6aa16344becf4a9993610a2c416
sorin1977 <39308303+sorin1977@users.noreply.github.com>
2019-05-01 21:08:15 +0300

    Update TestMessages.java

--- LambdaTests/src/com/ssn/sma/messages/TestMessages.java
+++ LambdaTests/src/com/ssn/sma/messages/TestMessages.java
@@ -3,8 +3,9 @@ package com.ssn.sma.messages;
 public class TestMessages {
 
 	public static void main(String[] args) {
-
+// maybe som changes are necesary
 		Message.send(mailer -> mailer.from().to().subject().content());
+		
 
 	}
 
